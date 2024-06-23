dcc6224de222d4b5334528e6ba308a65d3156df5
Sankalp Pimpalkar <SankalpPimpalkar23@gmail.com>
2024-06-23 12:10:39 +0530

    First push

---
+++ arrow.js
@@ -0,0 +1,12 @@
+// function print(string) {
+//     console.log(string)
+// }
+
+// const print = (string) => {
+//     console.log(string)
+// }
+// print("Hello World")
+
+    ;(() => {
+        console.log("Hello World")
+    })()
---
+++ sumOfNumbers.js
@@ -0,0 +1,10 @@
+let n = 10
+let sum = 0
+
+// for (let i = 1; i < n; i++){
+//     sum = sum + i
+// }
+
+sum = n*(n+1)/2
+
+console.log(sum)
